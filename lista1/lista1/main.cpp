c03dd3e57bc2422c0e190c561ba1e6a9281682ea
JanSnieg <olszowy.pawel@gmail.com>
2018-03-05 18:52:38 +0100

    dodane zadanie 6

--- lista1/lista1/main.cpp
+++ lista1/lista1/main.cpp
@@ -8,6 +8,9 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
+#include <iomanip>
+#include <cstdlib>
+#include <time.h>
 
 int zad1Rekurencyjnie(int x, int n)
 {
@@ -112,21 +115,71 @@ int zad5(int n, lnode *l)
     return 0;
 }
 
+void zad6(int N)
+{
+    int Z[N+1], minZ, maxZ;
+    int count = 0;
+    
+    srand((unsigned)time(NULL));
+    for(int i = 0; i < N; i++)
+        Z[i] = rand() % 10000;
+    if(N % 2)
+    {
+        count ++;
+        Z[N] = Z[N-1];
+    }
+    minZ = 10000;
+    maxZ = -1;
+    for(int i = 0; i < N; i += 2)
+        if(Z[i] > Z[i+1])
+        {
+            count ++;
+            if(Z[i]   > maxZ)
+            {
+                count ++;
+                maxZ = Z[i];
+            }
+            if(Z[i+1] < minZ)
+            {
+                count ++;
+                minZ = Z[i+1];
+            }
+        }
+        else
+        {
+            if(Z[i]   < minZ)
+            {
+                count ++;
+                minZ = Z[i];
+            }
+            if(Z[i+1] > maxZ)
+            {
+                count ++;
+                maxZ = Z[i+1];
+            }
+        }
+    for(int i = 0; i < N; i++)
+        std::cout << std::setw(4) << Z[i] << std::endl;
+    std::cout << std::endl <<  minZ << " : " << maxZ << std::endl;
+    std::cout << "Liczba porównań:\t" << count <<std::endl;
+}
+
 int main(int argc, const char * argv[]) {
-    std::cout << "Zad1:\t" << zad1Rekurencyjnie(2, 16) << std::endl;
-    std::cout << "Zad1:\t" << zad1Iteracyjnie(2, 16) << std::endl;
-    std::cout << "Zad2:\t" << zad2(-0.7, 0.5) << std::endl;
+//    std::cout << "Zad1:\t" << zad1Rekurencyjnie(2, 16) << std::endl;
+//    std::cout << "Zad1:\t" << zad1Iteracyjnie(2, 16) << std::endl;
+//    std::cout << "Zad2:\t" << zad2(-0.7, 0.5) << std::endl;
     double a[] = {1.,5.,7.,8.,1.,1.,4.,5.,7.,7.,7.,8.,10.,20.};
     int a_length = sizeof(a)/sizeof(a[0]);
-    std::cout << "Zad3: \nLiczba mnożeń:\t" << zad3(a, a_length, 3) << "\nDlugość wielomianu:\t" << a_length << std::endl;
-    std::cout << "Zad4:\t" << zad4(3, 20, 2) << std::endl;
+//    std::cout << "Zad3: \nLiczba mnożeń:\t" << zad3(a, a_length, 3) << "\nDlugość wielomianu:\t" << a_length << std::endl;
+//    std::cout << "Zad4:\t" << zad4(3, 20, 2) << std::endl;
     
     lnode third(3);
     lnode second(2, &third);
     lnode first(1, &second);
     
-    std::cout << "Zad5:\t" << zad5(1, &first) << "\t" << zad5(2, &first)
-    << "\t" << zad5(3, &first) << "\t" << zad5(4, &first) << std::endl;
+//    std::cout << "Zad5:\t" << zad5(1, &first) << "\t" << zad5(2, &first)
+//    << "\t" << zad5(3, &first) << "\t" << zad5(4, &first) << std::endl;
+    zad6(15);
 
     return 0;
 }
