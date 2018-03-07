925d1afee1f9d998fc8e2cfc44c3f1f4b0b94b6f
JanSnieg <olszowy.pawel@gmail.com>
2018-03-07 14:38:11 +0100

    lista 2 zadania 1-3 sprawdzone + notatki z wykladu

--- lista2/lista2/main.cpp
+++ lista2/lista2/main.cpp
@@ -4,12 +4,12 @@ int main ()
 {
     /*
      Zadanie 1
-     Ilość porównań przy szukaniu sposobem bisekcji to: log2(n)
+     Ilość porównań przy szukaniu sposobem bisekcji to: 2log2(n)
      
      Zadanie 2
         a. n-1 porównań, (n+1) * 4 bity.
-        b. n porównań, 20! * 4 bity
-        c. log2(n), log2(n) * 4 bity pamięci
+        b. n porównań, 2n * 4 bity
+        c. n porównań, n * 4 bity pamięci
      
      Zadanie 3
         a. n mnożeń (a1*b1 + a2*b2 + ... an*bn)
