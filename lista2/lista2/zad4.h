7b365059d75718ed5659316a017e9644d5c52e0b
JanSnieg <olszowy.pawel@gmail.com>
2018-03-14 09:11:22 +0100

    zad4 poprawione?

--- lista2/lista2/zad4.h
+++ lista2/lista2/zad4.h
@@ -12,15 +12,15 @@
 void zadanie4()
 {
     std::cout << "\n\n___ZADANIE 4___" << std::endl;
-    int n = 16; //wielkosc tablicy
-    int k = 3; //ile przesuwamy w prawo
-    int t[16] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    int n = 20; //wielkosc tablicy
+    int k = 10; //ile przesuwamy w prawo
+    int t[20] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+    int j = n-k;
     std::vector<int> zmienne;
-    for (auto i=0; i<k; i++)
+    for (auto i=0; i<j; i++)
     {
         zmienne.push_back(t[i]);
     }
-    int j = k;
     for (auto i=0; i<n; i++)
     {
         t[i] = t[j];
